b7caff62395f3fe37d0a9b7c11fbbaf022ca00c7
CDRAI <chholokdong@gmail.com>
2022-07-19 23:39:48 +0545

    leetcode problems

--- day_2/richestCutomer.js
+++ day_2/richestCutomer.js
@@ -1,11 +1,39 @@
-const richestCustomerWealth = (arr)=>{
-return Math.max(...arr.map(customer=>{
-    return customer.reduce((acc,curr)=>{
-        return acc +=curr
-    },0)
-}))
-}
 
-const returnValue = richestCustomerWealth([[1,2,3],[3,2,1]])
 
-console.log(returnValue)
+const richestCustomerWealth = (arr) => {
+  return Math.max(
+    ...arr.map((customer) => {
+      return customer.reduce((acc, curr) => {
+        return (acc += curr);
+      }, 0);
+    })
+  );
+};
+
+// optimum solution
+
+// const richestCustomerWealth = (accounts) => {
+//     let currentMaxValue = 0;
+//     let currentWealth ;
+
+//     for (let i = 0; i < accounts.length ; i++) {
+//         currentWealth = 0
+//       for (let j = 0; j < accounts[i].length; j++) {
+//         currentWealth += accounts[i][j];
+//         }
+
+//       currentMaxValue = Math.max(currentMaxValue, currentWealth);
+
+//   }
+
+//     return currentMaxValue;
+// };
+
+const returnValue = richestCustomerWealth([
+  [1, 2, 3],
+  [3, 2, 2],
+  [7, 8],
+  [1, 2, 2, 1, 4, 1],
+]);
+
+console.log(returnValue);
