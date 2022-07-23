6a576f04aeb922e00037d6665ebfbe2629d2e52f
CDRAI <chholokdong@gmail.com>
2022-07-23 23:17:23 +0545

    climbingStairs

---
+++ day_4/climbingStairs.js
@@ -0,0 +1,11 @@
+const climbingStairsFunc = (n) => {
+  if (n == 1) return 1;
+
+  if (n == 2) return 2;
+
+  return climbingStairsFunc(n - 1) + climbingStairsFunc(n - 2);
+};
+
+const returnValue = climbingStairsFunc(4);
+
+console.log(returnValue);
